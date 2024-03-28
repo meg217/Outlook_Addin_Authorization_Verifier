7f8f2b0347abfec27eeb471799997de3fa6956fd
Zach <sierrazja@gmail.com>
2024-03-28 15:22:03 -0500

    fixing banner logic

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -110,7 +110,7 @@ function getBodyAsync() {
  * @param { String } body 
  */
 function getBannerFromBody(body) {
-  const banner_regex = /^(([Tt][Oo][Pp]\s*[Ss][Ee][Cc][Rr][Ee][Tt]|[Tt][Ss]|[Ss][Ee][Cc][Rr][Ee][Tt]|[Ss]|[Cc][Oo][Nn][Ff][Ii][Dd][Ee][Nn][Tt][Ii][Aa][Ll]|[Cc]|[Uu][Nn][Cc][Ll][Aa][Ss][Ss][Ii][Ff][Ii][Ee][Dd]|[Uu])(\/\/)?((\w*\s*)*)(\/\/)?((\w*\s*)*)($\n)?)/m;
+  const banner_regex = /^(([Tt][Oo][Pp] *[Ss][Ee][Cc][Rr][Ee][Tt]|[Tt][Ss]|[Ss][Ee][Cc][Rr][Ee][Tt]|[Ss]|[Cc][Oo][Nn][Ff][Ii][Dd][Ee][Nn][Tt][Ii][Aa][Ll]|[Cc]|[Uu][Nn][Cc][Ll][Aa][Ss][Ss][Ii][Ff][Ii][Ee][Dd]|[Uu])(\/\/)?((\w* *)*)(\/\/)?((\w* *)*))/m;
 
   const banner = body.match(banner_regex);
   console.log(banner);
