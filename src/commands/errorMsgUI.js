791835b2598b113dc34e137219389c678de88b2c
meg217 <117707592+meg217@users.noreply.github.com>
2024-04-03 19:48:30 -0500

    ui elements

--- src/commands/errorMsgUI.js
+++ src/commands/errorMsgUI.js
@@ -1,22 +1,28 @@
+Office.initialize = function (reason) {
+    mailboxItem = Office.context.mailbox.item;
+  };
 
 function bannerNullHandler(banner){
 
     if (banner == null) {
         console.log("banner is null");
-        const options = {
-        height: 30,
-        width: 20,
-        promptBeforeOpen: false,
-    };
-        Office.context.ui.displayDialogAsync('https://meg217.github.io/Outlook_Addin_Authorization_Verifier/src/commands/dialog.html', options);
-        //difference between errorMessage and informationalMessage?
+        
+        // the below commented out code opens a new window with an html page, maybe need this in the future but not right now
+        // const options = {
+        // height: 30,
+        // width: 20,
+        // promptBeforeOpen: false,
+        // };
+        // Office.context.ui.displayDialogAsync('https://meg217.github.io/Outlook_Addin_Authorization_Verifier/src/commands/dialog.html', options);
+        
+        
+        
+        //type can either be errorMessage or informationalMessage
         mailboxItem.notificationMessages.addAsync("NoSend", {
         type: "errorMessage",
         message: "Please enter a banner marking for this email.",
         });
 
-        //maybe shouln't de-allow event? instead make a dialog box show up? no just makes it stall and say working on request...
-        console.log("event should be denied");
         //event.completed({ allowEvent: false });
 
         event.completed(
@@ -25,16 +31,10 @@ function bannerNullHandler(banner){
             cancelLabel: "Add a location",
             commandId: "msgComposeOpenPaneButton",
             contextData: JSON.stringify({ a: "aValue", b: "bValue" }),
-            errorMessage: "Don't forget to add a meeting location.",
+            errorMessage: "Please enter a banner, banner error detected.",
             sendModeOverride: Office.MailboxEnums.SendModeOverride.PromptUser
         }
         );
-        
-        var errorElement = document.querySelector('div.Dialog1045-title');
-        var errorElement2 = document.querySelector('class.ms-Dialog-title title-758');
-
-        console.log(errorElement2);
-        console.log(errorElement);
 
 
         return;
