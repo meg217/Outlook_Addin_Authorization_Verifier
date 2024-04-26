170ae662ce4e877737c25b7e5cc2157a37f27ba3
JDMxSpartan <kevindehoyos66066@gmail.com>
2024-04-25 20:57:45 -0500

    delete some comments

--- src/commands/errorMsgUI.js
+++ src/commands/errorMsgUI.js
@@ -1,58 +1,43 @@
 //all of the error handlers with popup messages will be handled here
 
-function bannerNullHandler(banner, event){
+function bannerNullHandler(banner, event) {
+  if (banner == null) {
+    console.log("banner is null");
 
-    if (banner == null) {
-        console.log("banner is null");
-        
-        // the below commented out code opens a new window with an html page, maybe need this in the future but not right now
-        // const options = {
-        // height: 30,
-        // width: 20,
-        // promptBeforeOpen: false,
-        // };
-        // Office.context.ui.displayDialogAsync('https://meg217.github.io/Outlook_Addin_Authorization_Verifier/src/commands/dialog.html', options);
-        
-        
-        
-        //type can either be errorMessage or informationalMessage
-        // mailboxItem.notificationMessages.addAsync("NoSend", {
-        // type: "errorMessage",
-        // message: "Please enter a banner marking for this email.",
-        // });
+    //type can either be errorMessage or informationalMessage
+    // mailboxItem.notificationMessages.addAsync("NoSend", {
+    // type: "errorMessage",
+    // message: "Please enter a banner marking for this email.",
+    // });
 
-        //event.completed({ allowEvent: false });
+    //event.completed({ allowEvent: false });
 
-        event.completed(
-        {
-            allowEvent: false,
-             cancelLabel: "Ok",
-             commandId: "msgComposeOpenPaneButton",
-             contextData: JSON.stringify({ a: "aValue", b: "bValue" }),
-             errorMessage: "Please enter a banner, banner error detected.",
-        //     //underneath with enable the user to press send anyways, might need later
-             sendModeOverride: Office.MailboxEnums.SendModeOverride.PromptUser
-        }
-        );
+    event.completed({
+      allowEvent: false,
+      cancelLabel: "Ok",
+      commandId: "msgComposeOpenPaneButton",
+      contextData: JSON.stringify({ a: "aValue", b: "bValue" }),
+      errorMessage: "Please enter a banner, banner error detected.",
+      //     //underneath with enable the user to press send anyways, might need later
+      sendModeOverride: Office.MailboxEnums.SendModeOverride.PromptUser,
+    });
 
-
-        return;
-    }
+    return;
+  }
 }
 
 //CHANGE:
 function errorPopupHandler(errorMsg, event) {
-    console.log("error detected, handeling popup and setting allow event to false");
-    event.completed(
-    {
-        allowEvent: false,
-        cancelLabel: "Ok",
-        commandId: "msgComposeOpenPaneButton",
-        contextData: JSON.stringify({ a: "aValue", b: "bValue" }),
-        errorMessage: errorMsg,
-        //sendModeOverride: Office.MailboxEnums.SendModeOverride.PromptUser
-    }
-    );
-    return;
-
+  console.log(
+    "error detected, handeling popup and setting allow event to false"
+  );
+  event.completed({
+    allowEvent: false,
+    cancelLabel: "Ok",
+    commandId: "msgComposeOpenPaneButton",
+    contextData: JSON.stringify({ a: "aValue", b: "bValue" }),
+    errorMessage: errorMsg,
+    //sendModeOverride: Office.MailboxEnums.SendModeOverride.PromptUser
+  });
+  return;
 }
